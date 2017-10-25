82d091497f2b7097dda3f9bcd08b6f6e75eb5b92
minibear <lei.zhang.bupt08@gmail.com>
2017-10-25 23:43:05 +0800

    MOD:

--- spiders/basic.py
+++ spiders/basic.py
@@ -2,9 +2,11 @@
 import requests
 from lxml import etree
 from selenium import webdriver
+from items import Proxy
 
 
 class BasicProxySpider(object):
     """
     基础代理爬虫, 包括抓取页面, 验证, 持久化
     """
+    
